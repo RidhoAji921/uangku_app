de236591dba62152cb2c2c917c8fedbda7cda489
Ridho Aji <ridhoaji921@gmail.com>
2023-09-30 20:34:24 +0700

    change layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import './pemasukan_page.dart';
 import 'package:uangku_app/format_currency.dart';
 
+// ignore: must_be_immutable
 class HomeBodyPage extends StatelessWidget {
-  const HomeBodyPage({super.key, required this.uang, required this.pemasukanNow, required this.pengeluaranNow});
+  HomeBodyPage({super.key, required this.uang, required this.pemasukanNow, required this.pengeluaranNow});
   final double uang;
   final double pemasukanNow;
   final double pengeluaranNow;
+
+  Color borderColor = const Color.fromARGB(255, 53, 53, 53);
+  Color tabColor = const Color.fromARGB(255, 250, 250, 250);
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +21,19 @@ class HomeBodyPage extends StatelessWidget {
         ),
         Container(
           decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 236, 236, 236),
+              color: tabColor,
               border: Border.all(
-                color: Colors.red,
+                color: borderColor,
               ),
               borderRadius: const BorderRadius.all(Radius.circular(15))),
           width: 370,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: EdgeInsets.only(top: 5, left: 10),
-                child: Text("Uangku"),
+                child: Text("Uangku", style: TextStyle(fontWeight: FontWeight.w600),),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 0, left: 10),
@@ -38,10 +42,41 @@ class HomeBodyPage extends StatelessWidget {
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 0, left: 10, bottom: 10),
-                child: Text('${CurrencyFormat.convertToIdr(pemasukanNow, 2)}'),
-              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [ 
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(text: "Pemasukkan", style: TextStyle(color: Colors.green, fontSize: 20, fontWeight: FontWeight.w600)),
+                            WidgetSpan(child: Icon(Icons.add, color: Colors.green, size: 20,))
+                          ]
+                        )
+                      ),
+                      Text("${CurrencyFormat.convertToIdr(pemasukanNow, 2)}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+                    ]
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [ 
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(text: "Pengeluaran", style: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.w600)),
+                            WidgetSpan(child: Icon(Icons.remove, color: Colors.red, size: 20,))
+                          ]
+                        )
+                      ),
+                      Text("${CurrencyFormat.convertToIdr(pengeluaranNow, 2)}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700)),
+                    ]
+                  ),
+                ],
+              )
             ],
           ),
         ),
@@ -50,40 +85,9 @@ class HomeBodyPage extends StatelessWidget {
         ),
         Container(
           decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 236, 236, 236),
+            color: tabColor,
             border: Border.all(
-              color: Colors.red,
-            ),
-            borderRadius: const BorderRadius.all(Radius.circular(15))),
-          width: 370,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  Text("Pengeluaran bulan lalu"),
-                  Text("${CurrencyFormat.convertToIdr(pemasukanNow, 2)}", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-                  // Icon(Icons.add),
-                ],
-              ),
-              Column(
-                children: [
-                  Text("Pengeluaran bulan ini"),
-                  Text("${CurrencyFormat.convertToIdr(pengeluaranNow, 2)}", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),),
-                  // Icon(Icons.thumb_up),
-                ],
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 236, 236, 236),
-            border: Border.all(
-              color: Colors.red,
+              color: borderColor,
             ),
             borderRadius: const BorderRadius.all(Radius.circular(15))),
           width: 370,
