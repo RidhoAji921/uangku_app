f6b1674b457deeae0f378b9d57c1132466312014
Ridho Aji <ridhoaji921@gmail.com>
2024-01-25 18:14:38 +0700

    remove final

--- lib/providers/provider_uang.dart
+++ lib/providers/provider_uang.dart
@@ -2,7 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uangku_app/classes/uang.dart';
 
 class UangBloc extends Cubit<Uang>{
-  final Uang uang;
+  Uang uang;
   UangBloc(this.uang) : super(uang);
 
   void pemasukkan(double uang){
